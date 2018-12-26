2872914869ef32603a491df76c714505bb55fcdf
MMangus <mmangus@gmail.com>
2018-12-26 11:05:46 -0500

    commit before heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://mjmangus16:stump16@ds241493.mlab.com:41493/squalla-2",
-  secretOrKey: "SquallaSecret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
