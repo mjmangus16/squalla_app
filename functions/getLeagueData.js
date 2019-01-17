cfd3c328e46250b03eac77b819c4707cf2839fe9
MMangus <mmangus@gmail.com>
2019-01-17 11:10:59 -0500

    adjusted tagLeague's to give a tie to the player with the current higher tag.

--- functions/getLeagueData.js
+++ functions/getLeagueData.js
@@ -13,37 +13,55 @@ const getLeagueData = (league, round) => {
 };
 
 const tagLeague = (league, round) => {
-  let playersInLeague = [];
-  let sortedPlayers = [];
-  let tagsInPlay = [];
-  let rosterData = [];
+  let tagScores = [];
+  let tagData = [];
+  let updatedTagData = [];
+  let leagueRoster = league.roster;
+  let tags = [];
+
   for (let i = 0; i < round.scores.length; i++) {
-    if (league.roster.includes(round.scores[i].player)) {
-      playersInLeague.push(round.scores[i]);
-    }
-  }
-  playersInLeague.sort((a, b) => {
-    return a.score - b.score;
-  });
-  for (let i = 0; i < playersInLeague.length; i++) {
-    sortedPlayers.push(playersInLeague[i].player);
-  }
-
-  for (let i = 0; i < league.rosterData.length; i++) {
-    if (sortedPlayers.includes(league.rosterData[i].username)) {
-      tagsInPlay.push(league.rosterData[i].tag);
+    if (leagueRoster.includes(round.scores[i].player)) {
+      tagScores.push(round.scores[i]);
     }
   }
 
-  tagsInPlay.sort((a, b) => {
-    return a - b;
-  });
-
-  for (let i = 0; i < sortedPlayers.length; i++) {
-    rosterData.push({ username: sortedPlayers[i], tag: tagsInPlay[i] });
+  for (let j = 0; j < tagScores.length; j++) {
+    for (let y = 0; y < league.rosterData.length; y++) {
+      if (tagScores[j].player === league.rosterData[y].username) {
+        tagData.push(league.rosterData[y]);
+      }
+    }
   }
 
-  return rosterData;
+  for (let i = 0; i < tagScores.length; i++) {
+    if (tagScores[i].player === tagData[i].username) {
+      tagData[i].score = tagScores[i].score;
+    }
+  }
+
+  tagData.sort((a, b) => {
+    if (a.score === b.score) {
+      return a.tag - b.tag;
+    } else {
+      return a.score - b.score;
+    }
+  });
+
+  for (let i = 0; i < tagData.length; i++) {
+    tags.push(tagData[i].tag);
+  }
+
+  tags.sort((a, b) => a - b);
+
+  for (let i = 0; i < tagData.length; i++) {
+    tagData[i].tag = tags[i];
+  }
+
+  for (let i = 0; i < tagData.length; i++) {
+    updatedTagData.push({ players: tagData[i].username, tag: tagData[i].tag });
+  }
+
+  return updatedTagData;
 };
 
 module.exports = getLeagueData;
