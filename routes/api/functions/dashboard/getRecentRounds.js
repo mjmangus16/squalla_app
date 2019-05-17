b5e6aa68249dd1a7ef0906153f27fca3f129c281
MMangus <mjmangus16@yahoo.com>
2019-05-17 10:10:19 -0400

    refactored getRecentRounds.js to use ES6 methods

--- routes/api/functions/dashboard/getRecentRounds.js
+++ routes/api/functions/dashboard/getRecentRounds.js
@@ -1,21 +1,20 @@
 const getRecentRounds = (rounds, username) => {
   rounds = rounds.slice(0, 3);
+
   let myRounds = [];
 
-  for (let i = 0; i < rounds.length; i++) {
-    let data = {
-      date: rounds[i].date,
-      course: rounds[i].course
-    };
+  rounds.forEach(round => {
+    myRounds.push({
+      date: round.date,
+      course: round.course,
+      score: round.scores.map(score => {
+        if (score.username === username) {
+          return score.score;
+        }
+      })[0]
+    });
+  });
 
-    for (let y = 0; y < rounds[i].scores.length; y++) {
-      if (rounds[i].scores[y].username === username) {
-        data.score = rounds[i].scores[y].score;
-      }
-    }
-
-    myRounds.push(data);
-  }
   return myRounds;
 };
 
