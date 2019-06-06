da3c074bbfd6eef7ad60a1816e8487df53dbc6df
MMangus <mjmangus16@yahoo.com>
2019-06-06 18:56:28 -0400

    fixed bug in getRoundsPerFriend that was causing the function to not get the number of rounds correctly. reverted back to for loop

--- routes/api/functions/friends/getRoundsPerFriend.js
+++ routes/api/functions/friends/getRoundsPerFriend.js
@@ -5,50 +5,50 @@ const getRoundsPerFriend = (friends, rounds, username) => {
     wins: 0
   }));
 
-  myFriends.forEach(friend => {
-    rounds.forEach(round => {
-      let myScore = round.scores.filter(score => score.username === username)[0]
-        .score;
-      console.log(myScore);
-      round.scores.forEach(score => {
-        if (score.username === friend) {
-          if (round.holes >= 18) {
-            friend.rounds++;
-          } else {
-            friend.rounds = friend.rounds + 0.5;
-          }
+  // myFriends.forEach(friend => {
+  //   rounds.forEach(round => {
+  //     let myScore = round.scores.filter(score => score.username === username)[0]
+  //       .score;
+  //     console.log(myScore);
+  //     round.scores.forEach(score => {
+  //       if (score.username === friend) {
+  //         if (round.holes >= 18) {
+  //           friend.rounds++;
+  //         } else {
+  //           friend.rounds = friend.rounds + 0.5;
+  //         }
 
-          if (score.score < myScore) {
-            if (round.holes >= 18) {
-              friend.wins++;
-            } else {
-              friend.wins = friend.wins + 0.5;
-            }
-          }
-        }
-      });
-    });
-  });
-
-  // for (let i = 0; i < myFriends.length; i++) {
-  //   for (let y = 0; y < rounds.length; y++) {
-  //     let myScore;
-  //     for (let j = 0; j < rounds[y].scores.length; j++) {
-  //       if (rounds[y].scores[j].username === profile.username) {
-  //         myScore = rounds[y].scores[j].score;
-  //       }
-  //     }
-  //     for (let j = 0; j < rounds[y].scores.length; j++) {
-  //       if (myFriends[i].friend === rounds[y].scores[j].username) {
-  //         myFriends[i].rounds++;
-  //         let friendScore = rounds[y].scores[j].score;
-  //         if (myScore < friendScore) {
-  //           myFriends[i].wins++;
+  //         if (score.score < myScore) {
+  //           if (round.holes >= 18) {
+  //             friend.wins++;
+  //           } else {
+  //             friend.wins = friend.wins + 0.5;
+  //           }
   //         }
   //       }
-  //     }
-  //   }
-  // }
+  //     });
+  //   });
+  // });
+
+  for (let i = 0; i < myFriends.length; i++) {
+    for (let y = 0; y < rounds.length; y++) {
+      let myScore;
+      for (let j = 0; j < rounds[y].scores.length; j++) {
+        if (rounds[y].scores[j].username === username) {
+          myScore = rounds[y].scores[j].score;
+        }
+      }
+      for (let j = 0; j < rounds[y].scores.length; j++) {
+        if (myFriends[i].friend === rounds[y].scores[j].username) {
+          myFriends[i].rounds++;
+          let friendScore = rounds[y].scores[j].score;
+          if (myScore < friendScore) {
+            myFriends[i].wins++;
+          }
+        }
+      }
+    }
+  }
 
   return myFriends;
 };
