80cdb303bba588834271941b8ae08e79b21106e1
MMangus <mjmangus16@yahoo.com>
2019-06-06 18:34:10 -0400

    fixed bug in getPerformancePointsPerCourse where splice was not returning the top 10 most played rounds

--- routes/api/functions/performance/getPerformancePointsPerCourse.js
+++ routes/api/functions/performance/getPerformancePointsPerCourse.js
@@ -8,8 +8,6 @@ const getPerformancePointsPerCourse = profile => {
     rating: 0
   }));
 
-  console.log("After Map: ", coursesPerformance);
-
   for (let i = 0; i < coursesPerformance.length; i++) {
     for (let y = 0; y < rounds.length; y++) {
       if (coursesPerformance[i].course === rounds[y].course) {
@@ -25,8 +23,6 @@ const getPerformancePointsPerCourse = profile => {
     }
   }
 
-  console.log("Before Sort: ", coursesPerformance);
-
   coursesPerformance.sort((a, b) => {
     if (a.rounds < b.rounds) {
       return 1;
@@ -36,12 +32,10 @@ const getPerformancePointsPerCourse = profile => {
     }
     return 0;
   });
-  if (coursesPerformance.length > 10) {
-    coursesPerformance.splice(0, 10);
-    console.log("After Splice: ", coursesPerformance);
-  }
 
-  console.log("Before Return: ", coursesPerformance);
+  if (coursesPerformance.length > 10) {
+    coursesPerformance.splice(9, coursesPerformance.length);
+  }
 
   return coursesPerformance;
 };
